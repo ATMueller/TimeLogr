99bbd77578e8ed2d59089c0cefe20c44f9374fd8
Rick <woessnrw@mail.uc.edu>
2020-09-30 18:23:16 -0400

    Added and updated controller

--- src/main/java/com/timelogr/enterprise/EnterpriseApplication.java
+++ src/main/java/com/timelogr/enterprise/EnterpriseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EnterpriseApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(EnterpriseApplication.class, args);
     }
 
--- src/main/java/com/timelogr/enterprise/TimeLogrController.java
+++ src/main/java/com/timelogr/enterprise/TimeLogrController.java
@@ -3,12 +3,19 @@ package com.timelogr.enterprise;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+//ALL WEB FILES GO IN THE resources/templates DIRECTORY
 @Controller
+@RequestMapping("/")
 public class TimeLogrController {
 
     @RequestMapping("/")
     public String index(){
-        return "start";
+
+        return "index"; //STARTS PROGRAM IN THE resources/templates/index.html
+    }
+
+    @RequestMapping("/page1")
+        public String page1() {
+            return "page1";
     }
 }
